019c04aee5c891fe771080b48e8ec4892347e84b
kritkan118 <std.67122420118@ubru.ac.th>
2025-12-15 11:06:01 +0700

    install

---
+++ README.md
@@ -0,0 +1,4 @@
+#week04
+## chapter 03
+## Flask Template
+*เรียนรู้การใช้งาน Flask Template*
---
+++ server.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='My Home Page')
+
+@app.route('/about')
+def about():
+    title='About Page'
+    name='kritkan'
+    email='std.67122420118@ubru.ac.th'
+    phone='099-682-3419'
+    return render_template('about.html',
+                           title=title,
+                           name=name,
+                           email=email,
+                           phone=phone)
+
+@app.route('/favorite/foods')
+def favorite_foods():
+    title='Favorite Foods Page'
+    foods = ['ส้มตำ','ข้าวผัด','ไก่ย่าง','คอหมูย่าง','ก๋วยเตี๋ยวเรือ','ลาบเป็ด']
+    return render_template('favorite_foods.html',
+                           foods=foods,
+                           title=title)
+
+@app.route('/sports')
+def sports():
+    title='Favorite Sports Page'
+    sports = ['ฟุตบอล','ฟุตซอล','วอลเลย์บอล','แบดมินตัน','เปตอง']
+    return render_template('sports.html',
+                           sports=sports,
+                           title=title)
+
+@app.route('/greeting/<username>')
+def geeting(username):
+    title='Welcome Page'
+    return render_template('welcome.html',
+                           title=title,
+                           username=username)
